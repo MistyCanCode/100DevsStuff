bec1269732b84838cd13153e40d0e714a7e716ae
Misty Tomlin (OSV) <misty.tomlin.osv@fedex.com>
2022-05-27 16:57:25 -0500

    finished race and class

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -40,77 +40,132 @@ function getFetch(){
    ).then(classes => classes.forEach(piece => {
        console.log(piece)
        let valOne = piece.proficiency_choices[0]
-       console.log(valOne.from)
+       //console.log(valOne.from)
        let pickFrom = []
-       valOne.from.forEach(element => pickFrom.push(element.name + " "))
-       console.log(pickFrom)
+       valOne.from.forEach(element => pickFrom.push(element.name.replace('Skill:', '')))
        let valTwo = piece.proficiencies
-       console.log(valTwo)
        let proficiences = []
        valTwo.forEach(element => proficiences.push(element.name))
-       console.log(proficiences)
        let valThree = piece.saving_throws
-       console.log(valThree)
        let saving = []
        valThree.forEach(element => saving.push(element.name + " "))
-       console.log(saving)
        let valFour = piece.starting_equipment
-       console.log(valFour)
        let startingArray = []
        valFour.forEach(element => startingArray.push(element.equipment))
-       console.log(startingArray)
        let equipQuantity = []
        valFour.forEach(element => equipQuantity.push(element.quantity))
-       console.log(equipQuantity)
        let starting = []
        startingArray.forEach(element => starting.push(element.name))
-       console.log(starting)
-       let choseFrom = []
-       choseFrom = equipQuantity.map((e, i) => e + " " + starting[i])
-       console.log(choseFrom)
+       let startingEquipment = []
+       startingEquipment = equipQuantity.map((e, i) => e + " " + starting[i] + " ")
        let valFive = piece.starting_equipment_options
-       console.log(valFive)
+       let howMany = []
+       valFive.forEach(element => howMany.push(element.choose))
+       console.log(howMany)
        let equipOptions = []
-       valFive.forEach(element => equipOptions.push(element.from))
-       console.log(equipOptions)
+       valFive.forEach(element => (equipOptions.push(element.from.forEach(el =>equipOptions.push(el)))))
+       let newEquipOptions = []
+       for(let i = 0; i < equipOptions.length; i++){
+           if(equipOptions[i] !== undefined){
+                newEquipOptions.push(equipOptions[i])
+           }
+       }
+       console.log(newEquipOptions)
+       let eOptions = []
+       newEquipOptions.forEach(element => eOptions.push(element.equipment_option))
+       let newEOptions = []
+       for(let i = 0; i < eOptions.length; i++){
+           if(eOptions[i] !== undefined){
+               newEOptions.push(eOptions[i])
+           }
+       }
+       console.log(newEOptions)
+       let newEOptionsNum = []
+       newEOptions.forEach(el => newEOptionsNum.push(el.choose))
+       console.log(newEOptionsNum)
+       let newoptionsfrom = []
+       newEOptions.forEach(element => (newoptionsfrom.push(element.from)))
+       console.log(newoptionsfrom)
+       let optionnames = []
+       for(let i = 0; i < newoptionsfrom.length; i++){
+           if(newoptionsfrom[i] !== undefined){
+               optionnames.push(newoptionsfrom[i].equipment_category)
+           }
+       }
+       console.log(optionnames)
+       let newoptionnames = []
+       optionnames.forEach(el => newoptionnames.push(el.name))
+       console.log(newoptionnames)
+       let anotherOption = []
+       anotherOption = newEOptionsNum.map((e,i) => e + " " + newoptionnames[i] + " ") 
+       console.log(anotherOption)
        let from = []
-       equipOptions.forEach(element => from.push(element.equipment))
-       console.log(from)
+       newEquipOptions.forEach(element => (from.push(element.equipment)))
+       let fromEquip = []
+       for(let i = 0; i < from.length; i++){
+           if(from[i] !== undefined){
+               fromEquip.push(from[i])
+           }
+       }
+       console.log(fromEquip)
+       let equipName = []
+       fromEquip.forEach(element => (equipName.push(element.name)))
+       console.log(equipName)
        let numOfOptions = []
        valFive.forEach(element => numOfOptions.push(element.choose))
        console.log(numOfOptions)
        let newOptions = []
-       equipOptions.forEach(element => newOptions.push(element.name))
+       equipOptions.forEach(element => newOptions.push(element))//.name
        console.log(newOptions)
+       let startingOptions = []
+       startingOptions = numOfOptions.map((e,i) => e + " " + equipName[i] + " ")
+       console.log(startingOptions)
        let valSix = piece.subclasses
        console.log(valSix)
        let optionNum = piece.starting_equipment_options
        console.log(optionNum)
        let choseNum = piece.proficiency_choices[0].choose
        console.log(choseNum)
-       let fromArray = piece.proficiency_choices[0].from
-       console.log(fromArray)
-       var choices = []
        var profs = []
        var throws = []
-       var equipment =[]
        var options = []
        var subclasses = []
        //if(valOne >= 1){let valueOne = valOne.forEach(element => {choices.push(element.choose, element.from)})}
        //console.log(choices)
        
-       let valueTwo = valTwo.forEach(element => {profs.push(element.name)})
-       let valueThree = valThree.forEach(element => {throws.push(element.name)})
-       let valueFour = valFour.forEach(element => {equipment.push(element.name, element.quantity)})
-       let valueFive = valFive.forEach(element => {options.push(element.name)})
-       let valueSix = valSix.forEach(element => {subclasses.push(element.name)})
-       console.log(`name: ${piece.name}, hit die: ${piece.hit_die}, choose -${choseNum}- from: ${pickFrom}, proficiences: ${profs}. Saving throws: ${throws}. Starting Equipment: ${equipment}, starting equipment options: choose -${optionNum}- : ${options}. Subclasses, if any: ${subclasses}`)
+       valTwo.forEach(element => {profs.push(element.name)})
+       valThree.forEach(element => {throws.push(element.name)})
+       valFive.forEach(element => {options.push(element.name)})
+       valSix.forEach(element => {subclasses.push(element.name)})
+       console.log(`name: ${piece.name}, hit die: ${piece.hit_die}, choose -${choseNum}- from Skills: ${pickFrom}. Proficiences: ${profs} Saving throws: ${throws} Starting Equipment: ${startingEquipment}, starting equipment options, choose: ${startingOptions}, ${anotherOption} Subclasses, if any: ${subclasses}`)
 
        let selection = piece.index
        console.log(selection)
        const p = document.createElement('p')
-       p.textContent = `name: ${piece.name}, hit die: ${piece.hit_die}, choose -${choseNum}- from: ${pickFrom}, proficiences: ${profs}. Saving throws: ${throws}. Starting Equipment: ${equipment}, starting equipment options: choose -${optionNum}- : ${options}. Subclasses, if any: ${subclasses}`
+       p.textContent = `Hit die: ${piece.hit_die}`
+       const p2 = document.createElement('p')
+       p2.textContent = `Choose -${choseNum}- from: ${pickFrom}` 
+       const p3 = document.createElement('p')
+       p3.textContent = `Proficiences: ${profs}`
+       const p4 = document.createElement('p')
+       p4.textContent = `Saving throws: ${throws}`
+       const p5 = document.createElement('p')
+       p5.textContent = `Starting Equipment: ${startingEquipment}`
+       const p6 = document.createElement('p')
+       p6.textContent = `Starting equipment options, choose: ${startingOptions}, ${anotherOption}`
+       const p7 = document.createElement('p')
+       if(subclasses.length < 1){
+           p7.textContent = `Subclasses, if any: none`
+       }else{
+           p7.textContent = `Subclasses, if any: ${subclasses}`
+       }
        document.querySelector(`.${selection}`).appendChild(p)
+       document.querySelector(`.${selection}`).appendChild(p2)
+       document.querySelector(`.${selection}`).appendChild(p3)
+       document.querySelector(`.${selection}`).appendChild(p4)
+       document.querySelector(`.${selection}`).appendChild(p5)
+       document.querySelector(`.${selection}`).appendChild(p6)
+       document.querySelector(`.${selection}`).appendChild(p7)
    }))
 
        raceList.catch(err => console.log(`error: ${err}`))
